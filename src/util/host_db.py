d2b78d4efe35e4e6ff456c7197c0dde42840aa8e
bm78g <isntglutencakejustbread@gmail.com>
2026-05-17 13:59:46 -0500

    fix: close connection after done with database

--- src/util/host_db.py
+++ src/util/host_db.py
@@ -1,9 +1,15 @@
 import sqlite3
 
 def store_host(host):
-    conn = sqlite3.connect("data/network_topology.db")
+    conn = sqlite3.connect("data/network_topology.db", timeout=5)
     cursor = conn.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS HOSTS (mac CHAR(17), ipv4 VARCHAR(15), vendor VARCHAR(64))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS hosts (mac CHAR(17), ipv4 VARCHAR(15), vendor VARCHAR(64))")
+    cursor.execute("INSERT INTO hosts (mac, ipv4, vendor) VALUES (?, ?, ?)", (host.mac, host.ip, host.vendor))
 
-store_host(None)
+    conn.commit()
+    conn.close()
+
+def store_hosts(hosts):
+    for host in hosts:
+        store_host(host)
