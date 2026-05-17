bf562381c1dc2ed8fd294b1939ec115d21d3c341
bm78g <isntglutencakejustbread@gmail.com>
2026-05-17 12:43:53 -0500

    feat: test port scans

--- src/main.py
+++ src/main.py
@@ -3,11 +3,24 @@ import os
 from dotenv import load_dotenv
 from util.vendor_lookup import lookup_vendors
 from util.reverse_dns import *
-from util.port_scan import scan_hosts
+from util.port_scan import scan_hosts, scan_hosts_legacy
 from models import Host
+from datetime import datetime
 
 load_dotenv()
 target_subnet = os.getenv("TARGET_SUBNET")
+
+def get_scan_time(legacy=False, hosts=[]):
+    start_time = datetime.now()
+    if legacy:
+        scan_hosts_legacy(hosts)
+        print("Legacy scan ", end="")
+    else:
+        scan_hosts(hosts)
+        print("Concurrent scan ", end="")
+    end_time = datetime.now()
+    delta = end_time - start_time
+    print(f"elapsed {delta}")
 
 def main():
     # Send ARP broadcast
@@ -29,20 +42,22 @@ def main():
     # Node information aggregation
     vendors = lookup_vendors(nodes)
     dns = search_hosts(nodes)
-    scan_result = scan_hosts(nodes)
+
+    get_scan_time(True, nodes)
+    get_scan_time(False, nodes)
         
-    assert len(vendors) == len(dns) == len(scan_result)
+    # assert len(vendors) == len(dns) == len(scan_result)
 
-    hosts = []
-    for i in range(len(nodes)):
-        host = Host(nodes[i].psrc, nodes[i].hwsrc)
-        host.vendor = vendors[i]
-        host.domain = dns[i]
-        host.port_scan = scan_result[i]
-        hosts.append(host)
+    # hosts = []
+    # for i in range(len(nodes)):
+    #     host = Host(nodes[i].psrc, nodes[i].hwsrc)
+    #     host.vendor = vendors[i]
+    #     host.domain = dns[i]
+    #     host.port_scan = scan_result[i]
+    #     hosts.append(host)
 
-    for host in hosts:
-        print(vars(host))
+    # for host in hosts:
+    #     print(vars(host))
 
 if __name__ == "__main__":
     main()
