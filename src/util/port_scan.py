949be474c082d927e517e0a64350049f10532e9a
bm78g <isntglutencakejustbread@gmail.com>
2026-05-17 12:44:16 -0500

    fix: create individual scanner, add legacy scan for testing

--- src/util/port_scan.py
+++ src/util/port_scan.py
@@ -2,10 +2,10 @@ import nmap
 from concurrent.futures import ThreadPoolExecutor
 
 ports = [21, 22, 25, 53, 80, 443, 3306, 3389, 5432, 8080]
-scanner = nmap.PortScanner()
 
 def scan_ports(target):
     port_str = ','.join(str(p) for p in ports)
+    scanner = nmap.PortScanner()
     res = scanner.scan(target, port_str, arguments='-sV')
     
     result = []
@@ -27,3 +27,6 @@ def scan_hosts(hosts):
         scan_results = list(exec.map(scan_ports, hosts))
         return scan_results
     return []
+
+def scan_hosts_legacy(hosts):
+    return [scan_ports(host.psrc) for host in hosts]
