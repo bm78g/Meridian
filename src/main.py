796a32824b9b7d320c20a1a25432557434f915e9
bm78g <isntglutencakejustbread@gmail.com>
2026-05-17 14:47:27 -0500

    feat: scan monitor in loop

--- src/main.py
+++ src/main.py
@@ -19,40 +19,45 @@ def monitor_network():
 
     packet = ether / arp
 
-    res = srp(packet, timeout=2, verbose=1)[0]
+    while True:
+        res = srp(packet, timeout=2, verbose=1)[0]
 
-    print("\nDiscovered hosts:")
-    for _, received in res:
-        print(f"IP: {received.psrc}, MAC: {received.hwsrc}")
+        print("\nDiscovered hosts:")
+        for _, received in res:
+            print(f"IP: {received.psrc}, MAC: {received.hwsrc}")
 
-    print()
+        print()
 
-    nodes = []
-    for _, received in res:
-        nodes.append(received)
+        nodes = []
+        for _, received in res:
+            nodes.append(received)
 
-    # Node information aggregation
-    vendors = lookup_vendors(nodes)
-    dns = search_hosts(nodes)
-    scan_result = scan_hosts(nodes)
-        
-    assert len(vendors) == len(dns) == len(scan_result)
+        # Node information aggregation
+        vendors = lookup_vendors(nodes)
+        dns = search_hosts(nodes)
+        scan_result = scan_hosts(nodes)
+            
+        assert len(vendors) == len(dns) == len(scan_result)
 
-    hosts = []
-    for i in range(len(nodes)):
-        host = Host(nodes[i].psrc, nodes[i].hwsrc)
-        host.vendor = vendors[i]
-        host.domain = dns[i]
-        host.port_scan = scan_result[i]
-        hosts.append(host)
+        hosts = []
+        for i in range(len(nodes)):
+            host = Host(nodes[i].psrc, nodes[i].hwsrc)
+            host.vendor = vendors[i]
+            host.domain = dns[i]
+            host.port_scan = scan_result[i]
+            hosts.append(host)
 
-    store_hosts(hosts)
+        store_hosts(hosts)
+
+        # Free memory after each scan
+        for host in hosts:
+            del host
 
 def main():
-    banner = pyfiglet.figlet_format("Meridian Engine")
+    banner = pyfiglet.figlet_format("Meridian")
     print(banner)
 
-    while (True):
+    while True:
         choice = input("Select an operation:\n1) Monitor network\n2) Exit program\n")
         match choice:
             case "1":
