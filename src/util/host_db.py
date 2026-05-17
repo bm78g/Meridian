0d1e909c8239105e7a3042005bd1927bb42f3023
bm78g <isntglutencakejustbread@gmail.com>
2026-05-17 14:03:02 -0500

    feat: implement primary key into db

--- src/util/host_db.py
+++ src/util/host_db.py
@@ -4,7 +4,7 @@ def store_host(host):
     conn = sqlite3.connect("data/network_topology.db", timeout=5)
     cursor = conn.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS hosts (mac CHAR(17), ipv4 VARCHAR(15), vendor VARCHAR(64))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS hosts (mac CHAR(17) PRIMARY KEY, ipv4 VARCHAR(15), vendor VARCHAR(64))")
     cursor.execute("INSERT INTO hosts (mac, ipv4, vendor) VALUES (?, ?, ?)", (host.mac, host.ip, host.vendor))
 
     conn.commit()
